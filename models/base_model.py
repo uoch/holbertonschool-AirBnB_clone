95061a9dbe1cfb06fcc927a58321ad7900802ae2
uoch <chaouch.thameurtc61@gmail.com>
2023-02-20 20:08:43 +0100

    first_commit

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import uuid
+import datetime
+
+
+class BaseModel:
+    """
+    A base model that provides an ID and timestamp for other models to inherit from.
+    """
+
+    def __init__(self, id=None):
+        """
+        Initializes a new instance of the BaseModel class.
+
+        """
+        if id is not None:
+            self.id = str(id) + ' - ' + str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def save(self):
+        """
+        Updates the timestamp for the model to the current date and time.
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the model.
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the model.
+        """
+        return {'id': self.id, 'created_at': self.created_at.isoformat(),
+                'updated_at': self.updated_at.isoformat(), '__class__': type(self).__name__}
