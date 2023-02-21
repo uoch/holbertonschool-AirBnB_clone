344c9307843d4bf41f2f42b3f6ad919b68e921f9
uoch <chaouch.thameurtc61@gmail.com>
2023-02-21 19:43:47 +0100

    reload

---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+BaseModel = __import__('models.base_model').BaseModel
+"""_summary_
+    """
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+        return self.__objects
+
+    def save(self):
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            obj_dict = {}
+            for key, obj in FileStorage.__objects.items():
+                obj_dict[key] = obj.to_dict()
+            json.dump(obj_dict, f)
+    def reload(self): 
+        if __file_path is not None :
+            with open('data.json', 'r') as f:
+                self.__objects = json.load(f)
+        else:
+            exit()
+        
