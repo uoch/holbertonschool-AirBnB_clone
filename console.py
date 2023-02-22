819484b9113042343b0b798b9f053de4969c20a3
uoch <chaouch.thameurtc61@gmail.com>
2023-02-22 22:09:16 +0100

    consol

---
+++ console.py
@@ -0,0 +1,17 @@
+import cmd
+
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        'Quit command to exit the program'
+        print()
+        return True
+    def do_EOF (self, arg):
+        'Quit command to exit the program'
+        return print()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
